33d197cd7e4a4df42b73abd3d45f2bc75c7b29e4
Truong Tien Dat <truongtiendat@B160418s.local>
2021-08-27 22:41:25 +0700

    apply react js

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!, only: [:profile]
+
+  def profile
+  	@user = current_user
+  end
+
+  def index
+  	@users = User.all
+  	mails = @users.map(&:email)
+    render component: 'Users', props: { mails: mails }
+  end
+
+  private
+end
